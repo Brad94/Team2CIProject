43a783f255b41fcd676b0f1a5a0ba8d8420a4673
Brad <bjpollard94@gmail.com>
2017-06-12 17:30:49 +0100

    Initial Script working

--- Development/initial.sh
+++ Development/initial.sh
@@ -1,5 +1,13 @@
-aws ec2 create-security-group --group-name team2 --description "Academy:Team2:AWSCLI" --vpc-id [ID-HERE]
+#!/bin/bash
 
-aws ec2 authorize-security-group-ingress --group-name team2 --protocol tcp --port 22 --cidr 0.0.0.0/0
+aws ec2 create-security-group --group-name team2DEV --description "Academy:Team2:AWSCLI" --vpc-id vpc-dbd86eb2
 
-aws ec2 run-instances --image-id ami-f1d7c395 --security-groups team2 --count 1 --instance-type t2.micro --key-name [KEYNAME-HERE] --query 'Instances[0].InstanceId' --subnet-id [SUBNET-ID-HERE] --user-data file://my_script.txt
+
+SGName=$(aws ec2 describe-security-groups --filters Name=vpc-id,Values=vpc-dbd86eb2 Name=group-name,Values=team2DEV --query 'SecurityGroups[0].GroupId' --output text)
+
+echo $SGName
+
+aws ec2 authorize-security-group-ingress --group-id $SGName --protocol tcp --port 22 --cidr 0.0.0.0/0
+
+
+aws ec2 run-instances --tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production}]' --image-id ami-f1d7c395 --security-group-id $SGName --count 1 --instance-type t2.micro --key-name AcademyKeyPair --query 'Instances[0].InstanceId' --subnet-id subnet-7d578c06 --user-data file://AWSscript.txt
